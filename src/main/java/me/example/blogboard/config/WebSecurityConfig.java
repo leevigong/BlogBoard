d8b0a0a59822093feb0862029ef41b14ba689045
Daeun <dddddaeun88.88@gmail.com>
2024-07-13 02:22:01 +0900

    Feat: 로그아웃 처리

--- src/main/java/me/example/blogboard/config/WebSecurityConfig.java
+++ src/main/java/me/example/blogboard/config/WebSecurityConfig.java
@@ -5,15 +5,13 @@ import me.example.blogboard.service.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
@@ -25,47 +23,43 @@ public class WebSecurityConfig {
 
     // 1. 스프링 시큐리티 기능 비활성화
     @Bean
-    public WebSecurityCustomizer configure() {
+    public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
-                .requestMatchers(new AntPathRequestMatcher("/static/**"));
+                .requestMatchers("/static/**");
     }
 
     // 2. 특정 HTTP 요청에 대한 웹 기반 보안 구성
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
                 .authorizeHttpRequests(auth -> auth // 3. 인증, 인가 설정
-                        .requestMatchers(
-                                new AntPathRequestMatcher("/login"),
-                                new AntPathRequestMatcher("/signup"),
-                                new AntPathRequestMatcher("/user")
-                        ).permitAll()
+                        .requestMatchers("/login", "/signup", "/user").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(formLogin -> formLogin // 4. 폼 기반 로그인 설정
                         .loginPage("/login")
-                        .defaultSuccessUrl("/articles")
-                )
+                        .defaultSuccessUrl("/articles"))
                 .logout(logout -> logout // 5. 로그아웃 설정
                         .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true)
-                )
-                .csrf(AbstractHttpConfigurer::disable) // 6. csrf 비활성화
-                .build();
+                        .invalidateHttpSession(true))
+                .csrf(AbstractHttpConfigurer::disable); // 6. csrf 비활성화
+
+        return http.build();
     }
 
     // 7. 인증 관리자 관련 설정
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception{
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userService); // 8. 사용자 정보 서비스 설정
-        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
-        return new ProviderManager(authProvider);
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder
+                .userDetailsService(userService) // 8. 사용자 정보 서비스 설정
+                .passwordEncoder(bCryptPasswordEncoder());
+        return authenticationManagerBuilder.build();
     }
 
     // 9. 패스워드 인코더로 사용할 빈 등록
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/me/example/blogboard/controller/UserApiController.java
+++ src/main/java/me/example/blogboard/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.example.blogboard.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.example.blogboard.dto.AddUserRequest;
 import me.example.blogboard.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,5 +20,11 @@ public class UserApiController {
     public String signup(AddUserRequest request){
         userService.save(request); // 회원 가입 메서드 호출
         return "redirect:/login";  // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
